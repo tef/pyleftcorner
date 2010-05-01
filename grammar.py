4a5e8ce27527e63528ab93d794d10d7406897727
Thomas Figg <tef@icarus.local>
2010-05-01 15:54:40 +0100

    left corner bounded bottom up parsing with precedence and constructors

--- grammar.py
+++ grammar.py
@@ -1,12 +1,83 @@
 import re
 
 
+def main():
+    g = Grammar('g')
+
+    g.item = lift("pi")| "e"
+    g.item[0,ItemParseTree] = re.compile("\d+")
+
+    
+    constructor = OpParseTree
+    g.add[20,constructor] = (g.expr < 20) + "+" + (g.expr <= 20) 
+    g.sub[20,constructor] = (g.expr < 20) + "-" + (g.expr <= 20) 
+    g.mul[10,constructor] = (g.expr <= 10) + "*" + (g.expr < 10) 
+    g.div[10, constructor] = (g.expr <= 10) + "/" + (g.expr < 10)
+
+
+    def sub(name, p, args):
+        return args[1] # return the middle expression
+
+    g.subexpr[0, sub] = "(" + (g.expr <= 100) + ")"
+
+    g.expr[0, ExprParseTree] = g.subexpr | g.item | g.add | g.sub | g.mul | g.div
+
+    g.block = g.expr +"$"
+
+
+    def do(input):
+        t = Tokenizer(input)
+        print 'input', input
+        print 'tree', g.block.parse(t)
+        if t.items:
+            print 'leftovers', t
+        print
+
+    do(["1","*","2","+","1"])
+    do(["1","+","2","*","8"])
+    do(["1","+","2","+","1"])
+    do(["1","*","2","*","8"])
+    do("(1+2)*3")
+    do("(2)")
+    do("1+2-3/5")
+    do("1+2/3/5")
+
+
+
+# precedence functions
+class LE(object):
+    def __init__(self, c):
+        self.c = c
+
+    def accepts(self, o):
+        return o <= self.c
+
+    def __str__(self):
+        return "<=%s"%self.c
+
+class LT(object):
+    def __init__(self, c):
+        self.c = c
+
+    def accepts(self, o):
+        return o < self.c 
+
+    def __str__(self):
+        return "<%s"%self.c
+
+
+default_precedence=LE(100)
+
+# helper functions
 def lift(item):
+    "transform an object into a grammar object. i.e lift(2)"
     if isinstance(item, GrammarObject):
         return item
     else:
         return GrammarTerminal(item)
 
+
+#parse tree
 class ParseTree(object):
     def __init__(self, name, precedence, args):
         self.name = name
@@ -16,7 +87,7 @@ class ParseTree(object):
         self.args = args
 
     def __str__(self):
-        return "%s%d(%s)"%(self.name,self.precedence," ".join(str(x) for x in self.args))
+        return "%s(%s)"%(self.name," ".join(str(x) for x in self.args))
 
 class OpParseTree(ParseTree):
     def __init__(self, name, precedence, args):
@@ -25,8 +96,25 @@ class OpParseTree(ParseTree):
         self.args =  args;
 
     def __str__(self):
-        return "[%s %s %s]"%(self.args[0].args[0], self.args[1].arg, self.args[2].args[0])
+        return "[%s %s %s]"%(self.args[0], self.args[1].arg, self.args[2])
 
+class ItemParseTree(ParseTree):
+    def __init__(self, name, precedence, args):
+        self.name = name
+        self.precedence = precedence
+        self.args =  args;
+
+    def __str__(self):
+        return str(self.args)
+
+class ExprParseTree(ParseTree):
+    def __init__(self, name, precedence, args):
+        self.name = name
+        self.precedence = precedence
+        self.args =  args;
+
+    def __str__(self):
+        return "%s"%self.args
 
 class Terminal(object):
     def __init__(self, arg):
@@ -36,6 +124,8 @@ class Terminal(object):
     def __str__(self):
         return '<'+self.arg+'>'
 
+
+#Grammar Objects
 class GrammarObject(object):
     def __add__(self, item):
         return GrammarAnd(self, item)
@@ -56,6 +146,7 @@ class GrammarObject(object):
         return ()
 
 class GrammarTerminal(GrammarObject):
+    "A terminal within a grammar rule"
     def __init__(self, terminal):
         self.terminal = terminal
 
@@ -77,6 +168,7 @@ class GrammarTerminal(GrammarObject):
         return Terminal(left)
 
 class GrammarNonTerminal(GrammarObject):
+    "Any non terminal"
     def parse_left_corner(self, input,p):
         return None
 
@@ -84,6 +176,7 @@ class GrammarNonTerminal(GrammarObject):
         return None
 
 class GrammarRule(GrammarNonTerminal):
+    "A non terminal appearing on the right hand side"
     def __init__(self, grammar, name):
         self.grammar = grammar
         self.name = name
@@ -105,7 +198,7 @@ class GrammarRule(GrammarNonTerminal):
     def parse(self, input, precedence=None):
         if precedence is None:
             precedence = default_precedence
-        return self.grammar.parse_down(self.name, input, precedence)
+        return self.grammar.parse(self.name, input, precedence)
 
     def __str__(self):
         return "%s:%s" % (self.grammar._name, self.name)
@@ -142,6 +235,7 @@ class GrammarRule(GrammarNonTerminal):
         return Temp()
 
 class GrammarOr(GrammarObject):
+    "A | B | C. Represents directed choice of A or then B or then C"
     def __init__(self, a,b):
         self.rules = [lift(a),lift(b)]
 
@@ -180,6 +274,7 @@ class GrammarOr(GrammarObject):
     
 
 class GrammarAnd(GrammarNonTerminal):
+    "A sequence of grammar clauses in succession"
     def __init__(self, a,b):
         self.rules = [lift(a),lift(b)]
 
@@ -222,6 +317,7 @@ class GrammarAnd(GrammarNonTerminal):
         return rights
 
 class GrammarConstraint(GrammarNonTerminal):
+    "Normally a non terminal which must have a specific precedence"
     def __init__(self, rule, precedence):
         self.rule = rule
         self.precedence = precedence
@@ -244,28 +340,6 @@ class GrammarConstraint(GrammarNonTerminal):
     def parse(self, input, p):
         return self.rule.parse(input, self.precedence)
 
-class LE(object):
-    def __init__(self, c):
-        self.c = c
-
-    def accepts(self, o):
-        return o <= self.c
-
-    def __str__(self):
-        return "<=%s"%self.c
-
-class LT(object):
-    def __init__(self, c):
-        self.c = c
-
-    def accepts(self, o):
-        return o < self.c 
-
-    def __str__(self):
-        return "<%s"%self.c
-
-default_precedence=LE(100)
-
 class Grammar():
     def __init__(self, name):
         self._rules = {}
@@ -275,6 +349,7 @@ class Grammar():
 
 
     def _add(self,name, p, val, c = ParseTree):
+        "internal method for adding a rule"
         v = lift(val)
         self._add_corner(name, v)
         if name not in self._rules:
@@ -282,16 +357,15 @@ class Grammar():
         self._rules[name].append((p,c,v))
 
     def _add_corner(self, name, val):
+        "index the left corners of each rule added. this is used to guide bottom up parsing"
         if name not in self._corners:
             self._corners[name] = set()
             self._corners[name].add(name)
 
         corners = val.corner_names()
         if corners:
-            print name , 'adding corners', corners
             self._corners[name].update(corners)
             for c in corners:
-                print c
                 if c in self._corners:
                     self._corners[name].update(self._corners[c])
                 if c not in self._corners_idx:
@@ -314,22 +388,21 @@ class Grammar():
     def __str__(self):
         return self._name+":\n\t"+"".join("%s%s --> %s\n\t"%((n[0],"[%s]"%n[1]) for n,r in self._rules.items()))
 
-    def parse_down(self, name, input, precedence):
-        #print '>parse_down', name , input
+    def parse(self, name, input, precedence):
+        #print '>parse', name , input
         while input.has_next() and self.parse_up(name, input,precedence):
-            pass
-        if True:
-            peek = input.peek()
-            if peek and isinstance(peek, ParseTree) and name == peek.name and precedence.accepts(peek.precedence):
-                #print 'parse_down< yes', name , input
-                return input.next()
-        #print 'parse_down<', name , input
+            pass # repeately grow the input until it cannot apply any more rules
+        peek = input.peek()
+        if peek and isinstance(peek, ParseTree) and name == peek.name and precedence.accepts(peek.precedence):
+            #print 'parse< yes', name , input
+            return input.next()
+        #print 'parse<', name , input
         return None
 
     def parse_up(self,topname,  input, precedence):
         #print '>parse_up', input
         peek = input.peek()
-        # change this to take any child left corner.
+        # for each of the non-terminals that can appear in the left corner of a top-name rule
         for name in self._corners[topname]:
             for p,c,r in self._rules[name]:
                 if precedence.accepts(p):
@@ -381,78 +454,7 @@ class Tokenizer(object):
     def __str__(self):
         return '[['+" ".join(str(x) for x in self.items)+']]'
 
-# fixme - make greedy?
-
-g = Grammar('g')
-
-g.item = lift("pi")| "e"
-g.item = re.compile("\d+")
-
-g.expr[20] = (g.expr < 20) + "+" + (g.expr <= 20) 
-g.expr[20] = (g.expr < 20) + "-" + (g.expr <= 20) 
-g.expr[10] = (g.expr <= 10) + "*" + (g.expr < 10) 
-g.expr[10] = (g.expr <= 10) + "/" + (g.expr < 10) 
-g.expr = "(" + (g.expr <= 100) + ")" | g.item
-
-g.block = g.expr +"$"
-
-g.unary[5] = "-" + (g.expr <= 5)
-
-#g.expr[90] = g.add
-
-# do we want *some* ambiguity? i.e in parsing up?
 
 
-def do(input):
-    t = Tokenizer(input)
-    print 'input', input
-    print 'tree', g.block.parse(t)
-    if t.items:
-        print 'leftovers', t
-    print
 
-do(["1","*","2","+","1"])
-do(["1","+","2","*","8"])
-do(["1","+","2","+","1"])
-do(["1","*","2","*","8"])
-do("(1+2)*3")
-do("(2)")
-do("1+2-3/5")
-do("1+2/3/5")
-
-
-"""
-bugs: no indirect left recursion 
-bugs: cannot handle ambiguous 'during' parses.
-
-tree climing -- when to stop
-
-    if I have expr + expr , expr * exp
-    I want to to return longest match that is an expr.
-    if I have expr + item
-    I want it to not do item -> expr as well then it will
-    never parse successfully.
-
-Solution a: cap the growth up to the name at the top?
-    removes left indirect recursion though.
-
-Solution b: handle non determinism
-
-precedence climbing 
-
-top down parsing - rewrite from the top down --> expr 
-bottom up parsing - replace 1 + 2 -> num + num --> expr
-lr - left to right, right most. i.e (1 + 2) + 3
-ll - left to right, left most i.e 1 + (2 + 3)
-egan stack - top down precedence parser with explicit stack for operations/items
-pratt parser - top down parser with precedence and climbing
-
-left corner / early parsing.
-
-given rule a ---> fooBARbaz , bottom up on foo, top down on baz. i.e
-
-when BAR, can we get foo from the left hand side
-
-thing is, this is just pegs with left recursion. i.e bottom up + dcg + memoization
-
-"""
+if __name__ == '__main__': main()
